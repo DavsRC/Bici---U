c1c7ba2c565433cf225738dc87e8250df02ab8d8
Davinson <Davinson.rincon@est.iudigital.edu.co>
2022-07-15 23:23:54 -0500

    Bici class has been added

---
+++ src/main/java/Models/Bici.java
@@ -0,0 +1,18 @@
+package Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Bici {
+
+    private String id;
+    private String type;
+    private String color;
+    private boolean isLent;
+}
