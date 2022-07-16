8395fa0a400e2f28e234dba63d74298f00d533b7
Davinson <Davinson.rincon@est.iudigital.edu.co>
2022-07-16 11:53:14 -0500

    Status Enum and Ticket class have been added

---
+++ src/main/java/Models/Ticket.java
@@ -0,0 +1,27 @@
+package Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Ticket {
+
+    private String code;
+    private Bici bici;
+    private User user;
+    private LocalDate date;
+    private LocalTime startDate;
+    private LocalTime endDate;
+    private boolean helmet;
+    private boolean noDamage;
+    private int amount;
+    private Status status;
+}
