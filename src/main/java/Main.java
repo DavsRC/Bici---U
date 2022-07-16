c79347383f1415db293df8e49b7fafc43a62f421
Davinson <Davinson.rincon@est.iudigital.edu.co>
2022-07-16 00:06:27 -0500

    Bicis data has been added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,14 @@
-import Operations.UserOperations;
+import Operations.OperationsBici;
 
 public class Main {
     public static void main(String[] args) {
 
 
-        UserOperations userOperations = new UserOperations();
-        userOperations.addUser();
+//        UserOperations userOperations = new UserOperations();
+//        userOperations.addUser();
+        OperationsBici operationsBici = new OperationsBici();
+        System.out.println(operationsBici.getBiciesList());
+
 
     }
 }
---
+++ src/main/java/Operations/IOperationsBici.java
@@ -0,0 +1,10 @@
+package Operations;
+
+import Models.Bici;
+
+import java.util.List;
+
+public interface IOperationsBici {
+
+    List<Bici> getBiciesList();
+}
